4d4a64a2df21f7a5c470394f910f59e74aad2ef8
Erick <dev.llerenas@gmail.com>
2020-08-06 17:56:06 -0500

    ps de todo

--- lib/screens/how_it_works.dart
+++ lib/screens/how_it_works.dart
@@ -10,12 +10,15 @@ class HowItWorks extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-          margin: EdgeInsets.only(left: 40, right: 40, top: 20),
+          margin: EdgeInsets.all(30),
           child: Column(
             children: <Widget>[
               Container(
                   height: MediaQuery.of(context).size.height / 3,
                   child: SvgPicture.asset('assets/question.svg')),
+                  SizedBox(
+                    height: 30,
+                  ),
               Text(
                 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non auctor tortor, vel molestie neque. Quisque a tellus sed dolor laoreet ultrices. Aliquam erat volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce vel diam gravida augue pharetra ultrices. Quisque tincidunt neque nec fermentum iaculis. Vivamus euismod efficitur diam. Sed sed massa ultrices, porttitor enim a, commodo odio. Maecenas placerat est turpis, a volutpat mi faucibus sed. Pellentesque faucibus, nibh a lacinia accumsan, nibh arcu pretium magna, in dignissim magna nulla pharetra neque. Nullam augue arcu, lobortis nec vehicula vestibulum, fringilla ut eros. In ut lectus arcu. Mauris ac nisl id tortor aliquam ullamcorper. Vivamus quis ultrices erat. Mauris eleifend tristique metus, non accumsan eros efficitur ut. Etiam pellentesque tincidunt posuere.',
                 textAlign: TextAlign.justify,
