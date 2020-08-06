6cf827cf79c40136c52fe239ee030153d30bd94e
Norman <normanLau@users.noreply.gitlabhmhsistemas.ddns.net>
2020-08-06 02:55:12 -0500

    First Commit

--- lib/screens/ads_screen.dart
+++ lib/screens/ads_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:applovin/applovin.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AdsScreen extends StatefulWidget {
   @override
@@ -23,13 +24,66 @@ class _AdsScreenState extends State<AdsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  Center(
-          child: RaisedButton(
-        onPressed: () => AppLovin.requestInterstitial(
-            (AppLovinAdListener event) => listener(event, true),
-            interstitial: true),
-        child: Text('Ver video'),
-      )),
+      body: Container(
+          margin: EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: SvgPicture.asset('assets/invest.svg')),
+              SizedBox(
+                height: 20,
+              ),
+              ButtonTheme(
+                minWidth: double.infinity,
+                height: 60,
+                child: RaisedButton(
+                  textColor: Colors.white,
+                  color: Colors.yellow[600],
+                  onPressed: () {},
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        '+25',
+                        style: TextStyle(fontSize: 24),
+                      ),
+                      Text(
+                        'Recoger Recompensa Diaria',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ButtonTheme(
+                minWidth: double.infinity,
+                height: 120,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  onPressed: () => AppLovin.requestInterstitial(
+                      (AppLovinAdListener event) => listener(event, true),
+                      interstitial: true),
+                  child: Column(
+                    children: <Widget>[
+                      Text(
+                        '+10',
+                        style: TextStyle(fontSize: 38),
+                      ),
+                      Text(
+                        'Ganar Puntos',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
---
+++ lib/screens/how_it_works.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class HowItWorks extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('¿Cómo Funciona?'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(left: 40, right: 40, top: 20),
+          child: Column(
+            children: <Widget>[
+              Container(
+                  height: MediaQuery.of(context).size.height / 3,
+                  child: SvgPicture.asset('assets/question.svg')),
+              Text(
+                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non auctor tortor, vel molestie neque. Quisque a tellus sed dolor laoreet ultrices. Aliquam erat volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce vel diam gravida augue pharetra ultrices. Quisque tincidunt neque nec fermentum iaculis. Vivamus euismod efficitur diam. Sed sed massa ultrices, porttitor enim a, commodo odio. Maecenas placerat est turpis, a volutpat mi faucibus sed. Pellentesque faucibus, nibh a lacinia accumsan, nibh arcu pretium magna, in dignissim magna nulla pharetra neque. Nullam augue arcu, lobortis nec vehicula vestibulum, fringilla ut eros. In ut lectus arcu. Mauris ac nisl id tortor aliquam ullamcorper. Vivamus quis ultrices erat. Mauris eleifend tristique metus, non accumsan eros efficitur ut. Etiam pellentesque tincidunt posuere.',
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 18, color: Colors.grey[700]),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
